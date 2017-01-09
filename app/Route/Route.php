ea4fca445827eddbeaee0719495bc24891123fcb
DragK <mlodyz.97@outlook.com>
2017-01-09 22:50:25 +0100

    Updating tests and small improvments to the Route

--- app/Route/Route.php
+++ app/Route/Route.php
@@ -6,6 +6,8 @@ class Route
 {
     public $route = array();
 
+    private $name, $path, $args;
+
     public function __call($method, $args) 
     {
         if (is_callable($this, $method)) {
@@ -24,7 +26,32 @@ class Route
             return false;
         }
 
-        $this->route[$path] = $function;
+        $this->namePath($path);
+
+        $this->route[$this->name] = array(
+            'path' => $path,
+            'function' => $function
+        );
+        return true;
+    }
+
+    private function namePath($path)
+    {
+        $arr_path = explode('/', $path);
+
+        if (sizeof($arr_path) == 1) {
+            if ($arr_path[0] == '') {
+                $this->name = '';
+                return true;
+            }
+        }
+
+        for ($i = 0; $i < sizeof($arr_path); $i++) {
+            if ($arr_path[$i] != '') {
+               $this->name = trim($arr_path[$i], '\\');
+               break;
+           }
+        }
         return true;
     }
 
@@ -35,7 +62,11 @@ class Route
         }
 
         if ($this->isRoute($path)) {
-            return $this->route[$path]();
+            if ($this->args != "") {
+                return $this->route[$this->name]['function']($this->args);
+            } else {
+                return $this->route[$this->name]['function']();
+            }
         } else {
             echo 'Error 404';
         }
@@ -43,12 +74,47 @@ class Route
         return true;
     }
 
-    private function isRoute($method)
+    private function isRoute($path)
     {
-        if (array_key_exists($method, $this->route)) {
+        $this->getArgsFromPath($path);
+        $this->namePath($path);
+
+        //TODO Implements full support for REGEX
+
+        if ($this->name == '') {
+            return true;
+        }
+
+        if (preg_match($this->route[$this->name]['path'], $path)) {
+            return true;
+        }
+
+        if (array_key_exists($path, $this->route)) {
             return true;
         }
             
         return false;        
+    }
+
+    private function getArgsFromPath($path)
+    {
+        $arr_path = explode('/', $path);
+
+        if ($arr_path[0] == "") {
+            return false;
+        }
+
+        if (sizeof($arr_path) < 2) {
+            return false;
+        }
+
+        $this->args = array();
+
+        for ($i = 0; $i < sizeof($arr_path); $i++) {
+            $this->args[$arr_path[$i]] = $arr_path[$i + 1];
+            $i++;
+        }
+
+        return true;
     }
 }
--- app/config/routes.php
+++ app/config/routes.php
@@ -1,14 +1,24 @@
 <?php
 
+/*
+ * Use REGEX for more advanced routes
+ * Pattern: $route->addRoute('name', 'name-something', function ($var) {});
+ * example: $route->addRoute('user/id', 'user/id-[0-9]', function ($id) {});
+ */
+
 $route->addRoute('', function() {
     echo 'Strona główna';
 });
 
-$route->addRoute('siema', function() {
-    echo 'Witam';
-});
-
-$route->addRoute('contact', function () {
+$route->addRoute('/contact/', function () {
     new \RR\Controllers\Contact();
 });
 
+$route->addRoute('/user_id\/[0-9]/', function ($args) {
+    echo 'Strona uzytkownika o ID: '.$args['user_id'];
+});
+
+$route->addRoute('/name_id\/[0-9]+\/price\/[0-9]/', function ($args) {
+    echo 'Strona produktu o id: '.$args['name_id'].'<br />';
+    echo 'oraz cenie: '.$args['price'];
+});
